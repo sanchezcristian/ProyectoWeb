dd30017d6c8e7ba48b71b285776b2c8ae07fe362
Cristian Sanchez <Cristian Sanchez>
2016-08-05 00:18:19 -0300

    User Data Base

---
+++ src/comunidadIT/DAO/ConnectionDB.java
@@ -0,0 +1,63 @@
+package comunidadIT.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import com.mysql.*;
+
+public class ConnectionDB {
+
+	private final String userName = "root";
+	private final String password = "root";
+	private final String dbName = "empresa";
+	private final String driver = "com.mysql.jdbc.Driver";
+	private final String url = "jdbc:mysql://localhost:3306/";
+	private Connection connection = null;
+
+	public ConnectionDB() {
+
+		try {
+			Class.forName(driver).newInstance();
+			connection = (Connection) DriverManager.getConnection(url + dbName, userName, password);
+			if (connection != null) {
+				System.out.println("Data base connection working using TCP/IP: " + url);
+			}
+		} catch (Exception e) {
+			System.out.println("ERROR: Exeption " + e);
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException sqle) {
+				System.out.println("ERROR: Exeption " + sqle);
+			}
+		}
+	}
+
+	public ResultSet getQuery(String query) {
+		Statement state = null;
+		ResultSet result = null;
+		try {
+			state = (Statement) connection.createStatement();
+			result = state.executeQuery(query);
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+		return result;
+	}
+
+	public void setQuery(String query) {
+
+		Statement state = null;
+
+		try {
+			state = (Statement) connection.createStatement();
+			state.execute(query);
+
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+	}
+}
--- src/comunidadIT/DAO/UserDAO.java
+++ src/comunidadIT/DAO/UserDAO.java
@@ -2,10 +2,12 @@ package comunidadIT.DAO;
 
 import comunidadIT.DB.DataBase;
 import comunidadIT.model.Registered;
+import comunidadIT.DAO.ConnectionDB;
 
 public class UserDAO {
 	
 	private static DataBase db = new DataBase();
+	private static ConnectionDB connectionDB = new ConnectionDB();
 	
 	public boolean addUser (Registered registered){
 		return (db.addUser(registered));
